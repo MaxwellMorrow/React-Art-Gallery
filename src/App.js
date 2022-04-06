714e896e00a3665c3439d4cef1bf6751c850047b
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-04-05 19:31:56 -0700

    finished activity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { useState, useEffect } from "react";
+import Gallery from "./components/Gallery";
+import ButtonBar from "./components/ButtonBar";
 function App() {
+  let [artId, setArtId] = useState(12720);
+  let [data, setData] = useState({});
+
+  useEffect(() => {
+    document.title = "Welcome to Artworld";
+    fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`
+    )
+      .then((response) => response.json())
+      .then((resData) => setData(resData));
+  }, [artId]);
+
+  // in App.js
+  // send this function down to <ButtonBar />
+  const handleIterate = (e) => {
+    setArtId(artId + Number(e.target.value));
+  };
+
+  /* Return JSX down here... */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gallery
+        objectImg={data.primaryImage}
+        artist={data.artistDisplayName}
+        title={data.title}
+      />
+
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
